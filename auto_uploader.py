cb519114ba1dfab4173061a59c8724ef4de6b463
Peter Buffon <pabuffon@gmail.com>
2022-04-29 12:56:59 -0700

    Update auto_uploader.py

--- auto_uploader.py
+++ auto_uploader.py
@@ -1,7 +1,20 @@
-from downloader import get_latest_episode, download
+import xml.etree.ElementTree as ET
+from collections import deque
+import requests
+
+from downloader import get_episode, download
 from normalizer import loudnorm
 from uploader import anchor_upload
 
+
+def get_titles(url):
+    r = requests.get(url)
+    root = ET.fromstring(r.content)
+    titles = set()
+    for title in root.findall("./channel/item/title"):
+        titles.add(title.text)
+    return titles
+    
 
 def norm(episode, filename, download_dir):
     input_path = download_dir + filename
@@ -17,10 +30,26 @@ def upload(episode, filename, download_dir):
         print(e, " , upload failed.")
 
 
+rss_url = "https://anchor.fm/s/6a41bc24/podcast/rss"
 channel = "https://www.youtube.com/channel/UC3b1prOA-wTLQ5EFJEqQh-A"
 download_dir = "/tmp/"
 
-episode = get_latest_episode(channel)
-download(episode.video_id, download_dir)
-norm(episode, episode.video_id + ".m4a", download_dir)
-upload(episode, episode.video_id + "_norm.m4a", download_dir)
+titles = get_titles(rss_url)
+episodes = deque()
+playlist_item = 1
+
+while True:
+    episode = get_episode(channel, playlist_item)
+    if episode.title in titles:
+        print(f"{episode.title} already uploaded...")
+        break
+    else:
+        playlist_item += 1
+        episodes.append(episode)
+
+while episodes:
+    episode = episodes.popleft()
+    download(episode.video_id, download_dir)
+    norm(episode, episode.video_id + ".m4a", download_dir)
+    upload(episode, episode.video_id + "_norm.m4a", download_dir)
+
